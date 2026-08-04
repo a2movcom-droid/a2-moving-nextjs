5379a2dd00f098d49df9dfde01d4ab5c9805f8cc
a2movcom-droid <a2movcom@gmail.com>
2026-08-04 12:37:11 -0700

    Declare canonical host in robots metadata

--- app/robots.ts
+++ app/robots.ts
@@ -1,2 +1,13 @@
+import type { MetadataRoute } from 'next';
 import { site } from '@/lib/site';
-export default function robots(){return {rules:{userAgent:'*',allow:'/'},sitemap:`${site.url}/sitemap.xml`}}
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${site.url}/sitemap.xml`,
+    host: site.url,
+  };
+}
